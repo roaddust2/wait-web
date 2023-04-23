8b0b71d4780c33c57dc0ff55c7c45cc56eff8691
roaddust2 <roaddust2@yahoo.com>
2023-04-23 13:45:16 +0400

    Change url name catalog->category

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,7 @@ import django.contrib.flatpages.views as fp_views
 urlpatterns = [
     path('', views.home.IndexView.as_view(), name='index'),
     path('catalog/', views.product.ProductListView.as_view(), name='catalog'),
-    path('catalog/<slug:category_slug>/', views.product.ProductCategorizedListView.as_view(), name='catalog'),
+    path('catalog/<slug:category_slug>/', views.product.ProductCategorizedListView.as_view(), name='category'),
     path('catalog/<slug:category_slug>/<slug:product_slug>', views.product.ProductDetailView.as_view(), name='product'),
     path('about/', fp_views.flatpage, {"url": "/about/"}, name="about"),
     path('payments/', fp_views.flatpage, {"url": "/payments/"}, name="payments"),
