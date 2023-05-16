8132c59a1b5d391cf927c8024dc0bdecb7ce03e4
roaddust2 <roaddust2@yahoo.com>
2023-05-16 15:13:19 +0400

    Remove Whitenoise

--- config/settings.py
+++ config/settings.py
@@ -47,7 +47,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
