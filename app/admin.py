212511bcb9327ec8fe524c7f32da855bbd2aa3d7
roaddust2 <roaddust2@yahoo.com>
2023-05-31 14:57:44 +0400

    Changed auth, use email instead of username

--- app/admin.py
+++ app/admin.py
@@ -13,6 +13,20 @@ from .models.product import Category, Product, ProductFeature, ProductImage
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
+    list_display = ('email', 'is_staff', 'is_active',)
+    list_filter = ('email', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
 
 
 # Internalized flatpages
