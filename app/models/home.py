ed95bfe390320cab0ddfad71690cf75be6df9362
roaddust2 <roaddust2@yahoo.com>
2023-08-17 11:06:59 +0400

    Create AbstractCompressImage

--- app/models/home.py
+++ app/models/home.py
@@ -2,8 +2,10 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from utils.optimizer import compress
 
+from app.models.abstract import AbstractCompressImage
 
-class CarouselItem(models.Model):
+
+class CarouselItem(AbstractCompressImage):
     """Carousel slide object to display on home page"""
 
     label = models.CharField(_('Label'), max_length=255)
@@ -13,11 +15,6 @@ class CarouselItem(models.Model):
     image = models.ImageField(_('Image'), upload_to='static/images/carousel/')
     image_alt = models.CharField(_('ImageAlt'), max_length=255, null=True, blank=True)
     priority = models.IntegerField(_('Priority'), default=1)
-
-    def save(self, *args, **kwargs):
-        new_image = compress(self.image)
-        self.image = new_image
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.label
