8a12fd1cbc7e82998c7611e3b0023158a9acb2ff
roaddust2 <roaddust2@yahoo.com>
2023-10-28 15:50:27 +0400

    Revert to default AbstractUser

--- app/models/user.py
+++ app/models/user.py
@@ -1,40 +1,5 @@
-from django.db import models
-from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
-from django.utils.translation import gettext_lazy as _
-
-
-class CustomUserManager(BaseUserManager):
-    """Custom user manager to use email instead of username for auth"""
-    def create_user(self, email, password, **extra_fields):
-        if not email:
-            raise ValueError(_('ErrorEmptyEmail'))
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save()
-        return user
-
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-        return self.create_user(email, password, **extra_fields)
 
 
 class CustomUser(AbstractUser):
-    username = None
-    email = models.EmailField('Email', unique=True)
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
-
-    objects = CustomUserManager()
-
-    def __str__(self):
-        return self.email
+    pass
